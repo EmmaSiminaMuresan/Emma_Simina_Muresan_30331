bd46e724500b103d8b0838750cc4aaf46435ca21
Emma Simina Muresan <simina.muresan@outlook.com>
2023-04-21 21:45:52 +0300

    homework

--- src/LAB4/App2/Main.java
+++ src/LAB4/App2/Main.java
@@ -5,6 +5,6 @@ public class Main {
         Integer monitor = 1;
         Integer monitor2 = 1;
         new ExecutionThread(monitor, monitor2, 0, 4, 2, 4, 4, 6).start();
-        new ExecutionThread(monitor, monitor2, 3, 5, 3, 5, 5, 7).start();
+        new ExecutionThread(monitor2, monitor, 3, 5, 3, 5, 5, 7).start();
     }
 }
